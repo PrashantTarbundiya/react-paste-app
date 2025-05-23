51beba88448873a2fe05da220a163c2831a48704
PrashatTarbundiya <prashattarbundiya2@gmail.com>
2025-05-23 14:49:10 +0530

    Error of path solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Home from './components/Home';
 import Paste from './components/Paste';
 import ViewPaste from './components/ViewPaste';
